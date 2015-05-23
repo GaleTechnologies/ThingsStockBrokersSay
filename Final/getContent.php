1683320da4e99e828451761d37a229e5f9f38b82
Gerald <galeg@onid.oregonstate.edu>
2015-05-22 22:00:37 -0700

    First upload

---
+++ Final/getContent.php
@@ -0,0 +1,15 @@
+ <?php
+//© Copyright 2015 www.thingsstockbrokerssay.com - All Rights Reserved 
+//Property of Gale Technologies LLC www.galetechnologiesllc.com
+
+$myFile = "testing.txt";
+$fileContents = file($myFile);
+$a = $fileContents;
+$q = $_REQUEST["q"];
+
+$hint = "";
+$printOutput = $fileContents[$q] . "   " . $q ;
+$hint = $printOutput;
+
+echo $hint;
+?>
---
+++ Final/index.php
@@ -0,0 +1,100 @@
+<?php 
+//© Copyright 2015 www.thingsstockbrokerssay.com - All Rights Reserved 
+//Property of Gale Technologies LLC www.galetechnologiesllc.com
+
+$myFile = "testing.txt";
+$fileContents = file($myFile);
+$lineCount = count(file($myFile));
+$lineNumber = rand(0, ($lineCount - 1));
+$printOutput = $fileContents[$lineNumber];
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="shortcut icon" href="http://www.publicdomainpictures.net/pictures/40000/nahled/dollar-sign.jpg">
+    <title>Things Stock Brokers Say</title>
+    <meta name="keywords" content="stocks, finance, funny, wall street, wall, street, trading, bonds, capitalism, money, NYSE, hookers, drugs" />
+    <meta name="description" content="Real words spoken by stock brokers on Wall Street. Submit what you've heard to: stockbrokerssay@gmail.com" />
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+    <script type="text/javascript">var switchTo5x=true;</script>
+    <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript">stLight.options({publisher: "59407b10-d849-445a-9e1b-864683a46fc3", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-62870449-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <script type="text/javascript">
+    var usedNumbers = []; 
+
+    console.log("hello");
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    $(window).load(function () {
+      makeid();
+    });
+
+    function makeid() {
+
+        var str = getRandomInt(0,41);
+
+         var xmlhttp = new XMLHttpRequest();
+         xmlhttp.onreadystatechange = function() {
+             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                 document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+             }
+         }
+         xmlhttp.open("GET", "getContent.php?q="+str, true);
+         xmlhttp.send();
+    }
+</script>
+</head>
+
+<body>
+<center>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- ThingsStockBrokersSayTOP -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:728px;height:90px"
+         data-ad-client="ca-pub-3109028274128230"
+         data-ad-slot="3593274908"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    <div>
+        <span class='st_twitterfollow_large' displayText='Twitter Follow' st_username='stockbrokerssay'></span>
+        <span class='st_twitter_large' displayText='Tweet'></span>
+        <span class='st_facebook_large' displayText='Facebook'></span>
+        <span class='st_pinterest_large' displayText='Pinterest'></span>
+        <span class='st_stumbleupon_large' displayText='StumbleUpon'></span>
+        <span class='st_sharethis_large' displayText='ShareThis'></span>
+        <span class='st_email_large' displayText='Email'></span>
+        <span> <a class="hashTag" href="https://twitter.com/stockbrokerssay"><h1>#ThingsStockBrokersSay</h1></a></span>
+    </div>
+</center>
+
+
+    <div class="main" onclick="makeid()">
+        <center class="content">
+            <p><span id="txtHint"></span></p>
+        </center>
+    </div>
+
+
+<br><br>
+
+<div class="footer"><a class="footer" href="http://galetechnologiesllc.com/"><center>&copy; Copyright 2015 www.thingsstockbrokerssay.com - All Rights Reserved</center></a></div>
+
+</body>
+
+</html>
